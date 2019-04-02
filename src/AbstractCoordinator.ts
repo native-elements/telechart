0239f48773737dd0b0c9d27c73c09a18c5332751
Valery Shibanov <shibaon@gmail.com>
2019-04-02 21:23:32 +0300

    Fixed round glitches

--- src/AbstractCoordinator.ts
+++ src/AbstractCoordinator.ts
@@ -22,21 +22,21 @@ export abstract class AbstractCoordinator {
     }
 
     public getXValue(canvasX: number) {
-        return (this.borders.maxX - this.borders.minX) * canvasX / this.telecanvas.width + this.borders.minX
+        return Math.round((this.borders.maxX - this.borders.minX) * canvasX / this.telecanvas.width + this.borders.minX)
     }
 
     public getYValue(canvasY: number) {
         const tHeight = this.telecanvas.height - (this.bottomPadding + this.topPadding)
-        return (this.topPadding - canvasY) * (this.borders.maxY - this.borders.minY) / tHeight + this.borders.maxY
+        return Math.round((this.topPadding - canvasY) * (this.borders.maxY - this.borders.minY) / tHeight + this.borders.maxY)
     }
 
     public getCanvasX(value: number) {
-        return (value - this.borders.minX) / (this.borders.maxX - this.borders.minX) * this.telecanvas.width
+        return Math.round(((value - this.borders.minX) / (this.borders.maxX - this.borders.minX) * this.telecanvas.width) * 100) / 100
     }
 
     public getCanvasY(value: number) {
         const tHeight = this.telecanvas.height - (this.bottomPadding + this.topPadding)
-        return tHeight - (value - this.borders.minY) / (this.borders.maxY - this.borders.minY) * tHeight + this.topPadding
+        return Math.round((tHeight - (value - this.borders.minY) / (this.borders.maxY - this.borders.minY) * tHeight + this.topPadding) * 100 / 100)
     }
 
     public addColumn(column: Telecolumn) {
