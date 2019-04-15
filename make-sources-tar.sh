a2702a06371941a9a69be1731b26946a9cd667e8
Valery Shibanov <shibaon@gmail.com>
2019-04-15 20:34:41 +0300

    Very last setup

--- make-sources-tar.sh
+++ make-sources-tar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar -czf dist/sources.tar.gz --exclude="node_modules" --exclude="package-lock.json" --exclude="dist/telechart.min.js" * .gitignore
+tar -czf dist/sources.tar.gz --exclude="node_modules" --exclude="package-lock.json" --exclude="dist/telechart.min.js" --exclude="dist/sources.tar.gz" * .gitignore
